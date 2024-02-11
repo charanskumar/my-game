7fbb956b0fb46e630d0b2f45d1fba331b602d130
charanskumar <charansenthilk@gmail.com>
2024-02-11 16:57:35 -0500

    Assign variables including array of possible words

---
+++ app.js
@@ -0,0 +1,4 @@
+const possibleWords = ["Walter-White", "Gus-Fring", "Heisenberg", "Salamanca", "Skyler-White", "Los-Pollos", "Albuquerque", "Jesse-Pinkman", "Tuco-Salamanca", "Steve-Gomez", "Hank-Schrader", "Saul-Goodman", "Huell", "Madrigal", "Don-Eladio", "Juan-Bolsa", "mule", "dead-drop", "lawyer", "agent", "New-Mexico", "Gilligan", "cancer", "Mike-Ehrmantraut", "desert", "Holly", "Flynn", "Junior", "Marie-Schrader", "Todd", "Uncle-Jack", "methylamine", "Gale-Boetticher", "chemistry", "Gray-Matter", "illegal", "prison", "Jane-Margolis", "precursor", "rolling-lab", "Hector", "Lydia", "Badger", "Bogdan", "car-wash", "laundromat", "superlab", "Andrea", "Skinny-Pete", "Krazy-Eight", "Tyrus", "Kuby", "twins", "Ted-Beneke", "Gretchen", "Elliot", "Declan", "Wendy", "Victor", "Combo", "ricin", "empire", "cartel"];
+let prevLetterGuesses = [];
+let guessesRem = 10;
+let answer = "";
